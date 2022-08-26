431000ea17769b0549e953d78e6a73f8663c02ee
bonniegee <blg2144@columbia.edu>
2022-08-26 17:12:25 -0400

    remove unused mock generator

--- tests/test_report_subjects.py
+++ tests/test_report_subjects.py
@@ -17,15 +17,6 @@ FIELDS = [
 ]
 
 
-def mock_subjects_generator():
-    with open(Path("fixtures", "subject_record.json")) as s:
-        subject = json.load(s)
-    count = 0
-    while count < 5:
-        count += 1
-        yield subject
-
-
 class TestSubjectReporter(unittest.TestCase):
     @patch("crons.aspace_client.ArchivesSpaceClient.all_subjects")
     @patch("crons.report_subjects.SubjectReporter.write_data_to_sheet")
@@ -35,7 +26,6 @@ class TestSubjectReporter(unittest.TestCase):
         mock_write.return_value = None
         subject_reporter = SubjectReporter("local_settings.cfg.example")
         subject_reporter.fields = FIELDS
-        # mock_subjects = mock_subjects_generator()
         as_data = subject_reporter.get_sheet_data()
         self.assertTrue(as_data)
 
@@ -47,6 +37,5 @@ class TestSubjectReporter(unittest.TestCase):
         with open(Path("fixtures", "subject_record.json")) as s:
             subject = json.load(s)
         subject_row = subject_reporter.get_row(subject)
-        print(subject_row)
         self.assertEqual(len(subject_row), 10)
         self.assertEqual(subject_row[2], "lcsh")
