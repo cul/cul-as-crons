4379ffdcfa2f0ffa69050ecd7d409848d8e7538f
bonniegee <blg2144@columbia.edu>
2022-08-26 13:12:14 -0400

    add agent reporter

--- crons/report_agents.py
+++ crons/report_agents.py
@@ -6,7 +6,7 @@ from .google_sheets_client import DataSheet
 
 class AgentsReporter(BaseAsCron):
     def __init__(self, config_file):
-        super(AgentsReporter, self).__init__(config_file, "report_subjects_sheet")
+        super(AgentsReporter, self).__init__(config_file, "report_agents_sheet")
         logging.basicConfig(
             datefmt="%m/%d/%Y %I:%M:%S %p",
             format="%(asctime)s %(message)s",
@@ -16,27 +16,53 @@ class AgentsReporter(BaseAsCron):
                 logging.StreamHandler(),
             ],
         )
-        self.data_sheet = DataSheet(
-            self.google_token,
-            self.google_sheet,
-            self.config["Google Sheets"]["report_agents_range"],
-        )
+        self.fields = [
+            "uri",
+            "title",
+            "source",
+            "authority_id",
+            "is_linked_to_published_record",
+            "publish",
+            "last_modified_by",
+            "last_modified",
+        ]
 
-    def get_as_data(self):
-        self.data_sheet.clear_sheet
+    def get_sheet_data(self):
         spreadsheet_data = []
         spreadsheet_data.append(self.fields)
-        agent_rows = self.get_row_data()
-        for agent_row in agent_rows:
+        agent_data = self.get_row_data()
+        for agent in agent_data:
+            agent_row = self.construct_row(agent)
             spreadsheet_data.append(agent_row)
-        subject_count = len(spreadsheet_data) - 1
-        logging.info(f"Total agent records: {subject_count}")
-        self.data_sheet.append_sheet(spreadsheet_data)
-        logging.info(
-            f"{len(spreadsheet_data)} rows written to {self.data_sheet.spreadsheet_id}"
-        )
-        msg = f"{subject_count} records imported by {__file__}."
+        agent_count = len(spreadsheet_data) - 1
+        logging.info(f"Total agent records: {agent_count}")
+        self.write_data_to_sheet(spreadsheet_data)
+        msg = f"{agent_count} records imported by {__file__}."
         return msg
+
+    def write_data_to_sheet(self, sheet_data):
+        data_sheet = DataSheet(
+            self.google_access_token,
+            self.google_refresh_token,
+            self.google_client_id,
+            self.client_secret,
+            self.config["Google Sheets"]["report_agents_sheet"],
+            self.config["Google Sheets"]["report_agents_range"],
+        )
+        data_sheet.clear_sheet()
+        data_sheet.append_sheet(sheet_data)
+        return f"Posted {len(sheet_data)} rows to sheet."
+
+    def construct_row(self, row_data):
+        """Construct row to write to spreadsheet.
+
+        Args:
+            row_data (dict): data from ASpace to write to row
+
+        Returns:
+            list: ordered fields
+        """
+        return [row_data[field] for field in self.fields]
 
     def get_row_data(self):
         for agent in self.as_client.all_agents():
---
+++ tests/test_report_agents.py
@@ -0,0 +1,56 @@
+import json
+import types
+import unittest
+from pathlib import Path
+from unittest.mock import patch
+
+from crons.report_agents import AgentsReporter
+
+
+def mock_agents_generator():
+    with open(Path("fixtures", "agent_record.json")) as s:
+        agent = json.load(s)
+    count = 0
+    while count < 5:
+        count += 1
+        yield agent
+
+
+class TestAgentsReporter(unittest.TestCase):
+    @patch("crons.aspace_client.ArchivesSpaceClient.all_agents")
+    @patch("crons.report_agents.AgentsReporter.write_data_to_sheet")
+    @patch("crons.aspace_client.ArchivesSpaceClient.__init__")
+    def test_get_sheet_data(self, mock_as_init, mock_write, mock_agents):
+        mock_as_init.return_value = None
+        mock_write.return_value = None
+        agent_reporter = AgentsReporter("local_settings.cfg.example")
+        as_data = agent_reporter.get_sheet_data()
+        self.assertTrue(as_data)
+
+    @patch("crons.aspace_client.ArchivesSpaceClient.__init__")
+    def test_construct_row(self, mock_as_init):
+        mock_as_init.return_value = None
+        agent_reporter = AgentsReporter("local_settings.cfg.example")
+        agent_data = {
+            "uri": "/agents/corporate_entities/1579",
+            "title": "McDonnell & Sons",
+            "source": "naf",
+            "authority_id": "http://id.loc.gov/authorities/names/no2001020090",
+            "is_linked_to_published_record": True,
+            "publish": True,
+            "last_modified_by": "api_user",
+            "last_modified": "2021-11-12T12:33:21Z",
+        }
+        agent_row = agent_reporter.construct_row(agent_data)
+        self.assertEqual(len(agent_row), len(agent_reporter.fields))
+        self.assertIsInstance(agent_row[5], bool)
+
+    @patch("crons.aspace_client.ArchivesSpaceClient.all_agents")
+    @patch("crons.aspace_client.ArchivesSpaceClient.__init__")
+    def test_get_row_data(self, mock_as_init, mock_agents):
+        mock_as_init.return_value = None
+        mock_agents.return_value = mock_agents_generator()
+        agent_reporter = AgentsReporter("local_settings.cfg.example")
+        agents_rows = agent_reporter.get_row_data()
+        self.assertIsInstance(agents_rows, types.GeneratorType)
+        self.assertEqual(len([a for a in agents_rows]), 5)
