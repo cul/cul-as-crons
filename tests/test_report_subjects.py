b03689b483e7ae9e7c224c3e3bf369495c93a426
bonniegee <blg2144@columbia.edu>
2022-09-06 14:24:06 -0400

    remove test for deleted method

--- tests/test_report_subjects.py
+++ tests/test_report_subjects.py
@@ -1,6 +1,4 @@
-import json
 import unittest
-from pathlib import Path
 from unittest.mock import patch
 
 from crons.report_subjects import SubjectReporter
@@ -13,12 +11,3 @@ class TestSubjectReporter(unittest.TestCase):
         subject_reporter = SubjectReporter("local_settings.cfg.example")
         as_data = subject_reporter.create_report()
         self.assertTrue(as_data)
-
-    @patch("crons.aspace_client.ArchivesSpaceClient.__init__", return_value=None)
-    def test_get_row(self, mock_as_init):
-        subject_reporter = SubjectReporter("local_settings.cfg.example")
-        with open(Path("fixtures", "subject_record.json")) as s:
-            subject = json.load(s)
-        subject_row = subject_reporter.get_row(subject)
-        self.assertEqual(len(subject_row), 10)
-        self.assertEqual(subject_row[2], "lcsh")
