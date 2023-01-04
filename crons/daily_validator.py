605d3ceb00c5c6b280299f4ace0ee54072b86d87
Bonnie Gordon <blg2144@columbia.edu>
2023-01-04 16:31:50 -0500

    log instead of print

--- crons/daily_validator.py
+++ crons/daily_validator.py
@@ -30,16 +30,16 @@ class DailyValidator(object):
         )
 
     def run(self):
+        # TODO: we'll probably want to log changes somehow, so if a record hasn't been updated manually, we're not going through all the children again
         """Run validation across a repository for updated (or new) resource records."""
-        new_resources = self.as_client.get_new_resources(
-            2, yesterday_utc()
-        )  # change this to 48 hours
+        # new_resources = self.as_client.get_new_resources(2, 1669672800)
+        new_resources = self.as_client.get_new_resources(2, yesterday_utc())
         for resource in new_resources:
-            ResourceValidator(resource).run()
+            ResourceValidator().run(resource)
 
 
 class ResourceValidator(object):
-    def __init__(self, resource):
+    def __init__(self):
         logging.basicConfig(
             datefmt="%m/%d/%Y %I:%M:%S %p",
             format="%(asctime)s %(message)s",
@@ -57,13 +57,16 @@ class ResourceValidator(object):
             self.config["ArchivesSpace"]["username"],
             self.config["ArchivesSpace"]["password"],
         )
-        self.resource = resource
 
-    def run(self):
+    def run(self, resource):
         """Validates a resource record  in ArchivesSpace.
 
         Only validates records if they have a published record in CLIO.
+
+        Args:
+            resource (obj): ArchivesSnake JSONModel object for a resource record
         """
+        self.resource = resource
         print(f"checking if {self.resource.title} is in CLIO...")
         if self.published_in_clio():
             logging.info(
@@ -86,13 +89,18 @@ class ResourceValidator(object):
             print(f"{self.resource.title} is NOT in CLIO.")
 
     def check_children(self):
+        # TODO: do we want to run this every time? do we want to run this maybe over the weekend or on demand?
         children = self.as_client.get_all_children(self.resource)
+        count = 0
         for child in children:
+            count += 1
+            if count % 500 == 0:
+                logging.info(f"{count} child - {child['display_string']}")
             fields_with_whitespace = leading_trailing(child)
             for field in fields_with_whitespace:
                 self.as_client.update_aspace_field(child, field, child[field].strip())
                 child = self.as_client.get_json_response(child["uri"])
-                print(field, child["display_string"])
+                logging.info(f"{child['display_string']} stripped of whitespace")
             if child.get("title"):
                 without_linebreaks = (
                     child["title"].replace("\r\n", " ").replace("\n", " ")
@@ -102,6 +110,7 @@ class ResourceValidator(object):
                         child, "title", without_linebreaks
                     )
                     child = self.as_client.get_json_response(child["uri"])
+                    logging.info(f"Line breaks removed from {child['display_string']}")
             new_dates = []
             dates = child["dates"]
             if dates:
@@ -110,13 +119,14 @@ class ResourceValidator(object):
                         date = check_date(date)
                         new_dates.append(date)
                     if new_dates != dates:
-                        print(new_dates)
+                        logging.info(f"{child['display_string']} dates updated")
                         self.as_client.update_aspace_field(child, "dates", new_dates)
                         child = self.as_client.get_json_response(child["uri"])
                 except DateException as e:
                     raise e
 
     def published_in_clio(self):
+        """Checks if a collection is in CLIO."""
         try:
             if self.resource.json().get("id_0"):
                 bibid = self.resource.json()["id_0"]
@@ -130,6 +140,7 @@ class ResourceValidator(object):
             return False
 
     def dacs_compliance(self):
+        """Checks resource for DACS compliance."""
         validator = Validator("cul.json", None)
         result = validator.validate_data(self.resource.json())
         if result["valid"] is True:
@@ -147,21 +158,21 @@ class ResourceValidator(object):
                     if ua.ref == x["ref"]
                 ][0]
                 if match:
-                    print(f"Unpublished agent found: {ua.ref}")
+                    logging.info(f"Unpublished agent found: {ua.ref}")
                     self.as_client.publish_agent(ua)
 
     def remove_whitespace_resource(self):
         """Check all top level fields for whitespace only, delete."""
         fields_with_whitespace = empty_values(self.resource_json)
         for field in fields_with_whitespace:
-            print(field)
+            logging.info(field)
             self.as_client.update_aspace_field(self.resource_json, field, None)
 
     def remove_trailing_resource(self):
         """Check all top level fields for leading and trailing whitespace."""
         fields_with_whitespace = leading_trailing(self.resource_json)
         for field in fields_with_whitespace:
-            print(field)
+            logging.info(field)
             self.as_client.update_aspace_field(
                 self.resource_json, field, self.resource_json[field].strip()
             )
@@ -175,7 +186,7 @@ class ResourceValidator(object):
                     date = check_date(date)
                     new_dates.append(date)
                 if new_dates != dates:
-                    print(new_dates)
+                    logging.info(new_dates)
                     self.as_client.update_aspace_field(
                         self.resource_json, "dates", new_dates
                     )
