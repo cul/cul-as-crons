2c75c96aa395c7020128a0df9fd791608c3bfbdd
Bonnie Gordon <blg2144@columbia.edu>
2023-01-10 15:28:33 -0500

    add command line reporting script

---
+++ all_reports.py
@@ -0,0 +1,19 @@
+import argparse
+
+from crons.report_accessions import AccessionsReporter
+from crons.report_agents import AgentsReporter
+from crons.report_subjects import SubjectReporter
+from crons.resource_reporter import ResourceReporter
+
+
+def main():
+    """docstring for main"""
+    parser = argparse.ArgumentParser(
+        description="Generates reports against ArchivesSpace API"
+    )
+    parser.add_argument("--google_sheets", action="store_true")
+    args = parser.parse_args()
+    ResourceReporter().run(args.google_sheets)
+    AccessionsReporter().run(args.google_sheets)
+    AgentsReporter().run(args.google_sheets)
+    SubjectReporter().run(args.google_sheets)
