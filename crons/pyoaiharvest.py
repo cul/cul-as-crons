63ed927cf366bc3d961506421ef5accbfd59f28b
Bonnie Gordon <blg2144@columbia.edu>
2023-04-11 17:02:17 -0400

    correct code

--- crons/pyoaiharvest.py
+++ crons/pyoaiharvest.py
@@ -1,154 +1,137 @@
-# Script to harvest collection-level data from AS via OAI-PMH feed, and transform into Voyager-format MARCXML for nightly overlay.
+"""pyoaiharvester script for harvesting OAI-PMH 2.0 Repositories"""
 
-import argparse
-import datetime
-import logging
-import os
-import subprocess
+import urllib.request
+import zlib
 import time
+import re
+import xml.dom.pulldom
+import argparse
+
+N_DATA_BYTES, N_RAW_BYTES, N_RECOVERIES, MAX_RECOVERIES = 0, 0, 0, 3
 
 
-class OAIHarvester(object):
-    def __init__(self):
-        self.destination_folder = "/cul/cul0/ldpd/archivesspace/oai/"
-        logging.basicConfig(
-            datefmt="%m/%d/%Y %I:%M:%S %p",
-            format="%(asctime)s %(message)s",
-            level=logging.INFO,
-            handlers=[
-                logging.FileHandler(f"{self.destination_folder}oai_harvester.log"),
-                logging.StreamHandler(),
-            ],
-        )
-        self.pyoaiharvest = os.path.join(os.path.dirname(__file__), "pyoaiharvest.py")
-        self.oai_url = "https://aspace.library.columbia.edu/public/oai/"
+def get_file(server_string, command, verbose=1, sleep_time=0):
+    """Primary function for requesting OAI-PMH data from repository,
+       checking for errors, handling possible compression and returning
+       the XML string to the rest of the script for writing to a file."""
 
-    def run(self, today_string, harvest_all=False):
-        self.today_string = today_string
-        out_path_raw_all = self.harvest_all()
-        if harvest_all:
-            self.out_path_raw = out_path_raw_all
-        else:
-            self.out_path_raw = self.harvest_past_day()
-        self.voyager_transform
+    global N_RECOVERIES, N_DATA_BYTES, N_RAW_BYTES
+    if sleep_time:
+        time.sleep(sleep_time)
+    remote_addr = server_string + f'?verb={command}'
+    if verbose:
+        print("\r", f"get_file ...'{remote_addr[-90:]}'")
+    headers = {'User-Agent': 'pyoaiharvester/3.0', 'Accept': 'text/html',
+               'Accept-Encoding': 'compress, deflate'}
+    try:
+        req = urllib.request.Request(remote_addr, headers=headers)
+        with urllib.request.urlopen(req) as response:
+            remote_data = response.read()
+    except urllib.request.HTTPError as ex_value:
+        if ex_value.code == 503:
+            retry_wait = int(ex_value.hdrs.get("Retry-After", "-1"))
+            if retry_wait < 0:
+                return None
+            print(f'Waiting {retry_wait} seconds')
+            return get_file(server_string, command, 0, retry_wait)
+        if N_RECOVERIES < MAX_RECOVERIES:
+            N_RECOVERIES += 1
+            return get_file(server_string, command, 1, 60)
+        return None
+    N_RAW_BYTES += len(remote_data)
+    try:
+        remote_data = zlib.decompressobj().decompress(remote_data)
+    except zlib.error:
+        pass
+    remote_data = remote_data.decode('utf-8')
+    N_DATA_BYTES += len(remote_data)
+    error_code = re.search('<error *code=\"([^"]*)">(.*)</error>', remote_data)
+    if error_code:
+        print(f"OAIERROR: code={error_code.group(1)} '{error_code.group(2)}'")
+        return None
 
-    def harvest_all(self):
-        out_path_raw_all = os.path.join(
-            self.destination_folder, f"{self.today_string}.asAllRaw.xml"
-        )
-        self.oai_harvest(out_path_raw_all)
-        return out_path_raw_all
-
-    def harvest_past_day(self):
-        out_path_raw = os.path.join(
-            self.destination_folder, f"{self.today_string}.asRaw.xml"
-        )
-        yesterday = (datetime.date.today() - datetime.timedelta(days=1)).strftime(
-            "%Y%m%d"
-        )
-        self.oai_harvest(out_path_raw, from_date=yesterday)
-
-    def voyager_transform(self):
-        xslt_path = os.path.join(os.path.dirname(__file__), "../xslt/cleanOAI.xsl")
-        out_path_clean = os.path.join(
-            self.destination_folder, f"{self.today_string}.asClean.xml"
-        )
-        logging.info("Processing file with XSLT...")
-        process_xml = self.saxon_process(self.out_path_raw, xslt_path, out_path_clean)
-        logging.info(process_xml)
-
-    def oai_harvest(
-        self, out_path, from_date=None,
-    ):
-        cmd = [
-            "python",
-            self.pyoaiharvest,
-            "-l",
-            self.oai_url,
-            "-m",
-            "oai_marc",
-            "-s",
-            "collection",
-            "-o",
-            out_path,
-        ]
-        if from_date:
-            cmd.append("-f")
-            cmd.append(from_date)
-        p = subprocess.Popen(
-            [cmd],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            shell=True,
-        )
-        result = p.communicate()
-        if result[1]:
-            logging.error(result[1].decode("utf-8"))
-            return f"PYOAIHARVEST ERROR: {result[1].decode('utf-8')}"
-        else:
-            logging.info(result[0].decode("utf-8"))
-            return result[0].decode("utf-8")
-
-    def remove_old_files(self):
-        """Remove files from OAI directory that are over 30 days old."""
-        old = time.time() - 30 * 24 * 60 * 60
-        for f in os.listdir(self.destination_folder):
-            filepath = os.path.join(self.destination_folder, f)
-            if os.path.isfile(filepath):
-                stat = os.stat(filepath)
-                if stat.st_mtime < old:
-                    logging.info(f"removing: {filepath}")
-                    os.remove(filepath)
-
-    def saxon_process(
-        self, saxon_path, xml_file, xslt_file, out_file=None, params=None
-    ):
-        cmd = [
-            "java",
-            "-jar",
-            saxon_path,
-            xml_file,
-            xslt_file,
-            "--suppressXsltNamespaceCheck:on",
-        ]
-        if out_file:
-            cmd.append(f" > {out_file}")
-        if params:
-            cmd.insert(5, params)
-        p = subprocess.Popen(
-            cmd,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            shell=True,
-        )
-        result = p.communicate()
-        if not result[1]:
-            return result[0].decode("utf-8")
-        else:
-            raise Exception(result[1].decode("utf-8"))
-
-
-def main():
-    """Script to harvest collection-level data from AS via OAI-PMH feed and transform into Voyager-format MARCXML for nightly overlay.
-
-    Output is (a) asRaw.xml (deltas), (b) asAllRaw.xml (all records), and (c) asClean.xml,
-    transformed into Voyager MARCXML.
-
-    Options:
-    --HARVESTALL: ignore date params and get all records.
-    """
-    p = argparse.ArgumentParser(
-        description="Script to harvest collection-level data from AS via OAI-PMH feed, and transform into Voyager-format MARCXML for nightly overlay."
-    )
-    p.add_argument(
-        "--HARVESTALL", default=False, action="store_true", help="harvest all records?"
-    )
-    args = p.parse_args()
-    today_string = datetime.date.today().strftime("%Y%m%d")
-    OAIHarvester().run(today_string, args.HARVESTALL)
-
+    return remote_data
 
 if __name__ == "__main__":
-    main()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-l", "--link", dest="link",
+                        help="URL of repository")
+    parser.add_argument("-o", "--filename", dest="filename",
+                        help="write repository to file")
+    parser.add_argument("-f", "--from", dest="from_date",
+                        help="harvest records from this date yyyy-mm-dd")
+    parser.add_argument("-u", "--until", dest="until",
+                        help="harvest records until this date yyyy-mm-dd")
+    parser.add_argument("-m", "--mdprefix", dest="md_prefix", default="oai_dc",
+                        help="use the specified metadata format")
+    parser.add_argument("-s", "--setName", dest="setName",
+                        help="harvest the specified set")
+
+    args = parser.parse_args()
+
+    if args.link is None or args.filename is None:
+        parser.print_help()
+        parser.error("a repository url and output file are required")
+
+    if args:
+        SERVER_STRING = VERB_OPTS = FROM_DATE = UNTIL_DATE = MD_PREFIX = OAI_SET = ''
+        if args.link:
+            SERVER_STRING = args.link
+        if args.filename:
+            outFileName = args.filename
+        if args.from_date:
+            FROM_DATE = args.from_date
+        if args.until:
+            UNTIL_DATE = args.until
+        if args.md_prefix:
+            MD_PREFIX = args.md_prefix
+        if args.setName:
+            OAI_SET = args.setName
+    else:
+        parser.print_help()
+
+    if not SERVER_STRING.startswith('http'):
+        SERVER_STRING = 'https://' + SERVER_STRING
+
+    print(f"Writing records to {outFileName} from archive {SERVER_STRING}")
+
+    if OAI_SET:
+        VERB_OPTS += f'&set={OAI_SET}'
+    if FROM_DATE:
+        VERB_OPTS += f'&from={FROM_DATE}'
+    if UNTIL_DATE:
+        VERB_OPTS += f'&until={UNTIL_DATE}'
+
+    VERB_OPTS += f'&metadataPrefix={MD_PREFIX}'  # Defaults to oai_dc
+
+    print(f"Using url:{SERVER_STRING + '?ListRecords' + VERB_OPTS}")
+
+    with open(outFileName, "w", encoding="utf-8") as ofile:
+        ofile.write('<repository xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" \
+     xmlns:dc="http://purl.org/dc/elements/1.1/" \
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">\n')
+
+        data = get_file(SERVER_STRING, 'ListRecords' + VERB_OPTS)
+
+        RECORD_COUNT = 0
+
+        while data:
+            events = xml.dom.pulldom.parseString(data)
+            for (event, node) in events:
+                if event == "START_ELEMENT" and node.tagName == 'record':
+                    events.expandNode(node)
+                    node.writexml(ofile)
+                    RECORD_COUNT += 1
+            mo = re.search('<resumptionToken[^>]*>(.*)</resumptionToken>', data)
+            if not mo:
+                break
+            data = get_file(SERVER_STRING, f"ListRecords&resumptionToken={mo.group(1)}")
+
+        ofile.write('\n</repository>\n')
+        ofile.close()
+
+    print(f"\nRead {N_DATA_BYTES} bytes ({N_DATA_BYTES / N_RAW_BYTES:.2f} compression)")
+
+    print(f"Wrote out {RECORD_COUNT:,d} records")
