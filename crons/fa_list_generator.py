ca1f8b42ea23b2d3a7b174f9714adf3a96c721d2
Bonnie Gordon <blg2144@columbia.edu>
2023-04-19 13:09:55 -0400

    add logging, docstrings

--- crons/fa_list_generator.py
+++ crons/fa_list_generator.py
@@ -1,3 +1,4 @@
+import logging
 from configparser import ConfigParser
 from pathlib import Path
 
@@ -16,45 +17,64 @@ class FindingAidLists(object):
             self.config["ArchivesSpace"]["password"],
         )
         self.base_path = self.config["Other"]["finding_aids_lists"]
+        logging.basicConfig(
+            datefmt="%m/%d/%Y %I:%M:%S %p",
+            format="%(asctime)s %(message)s",
+            level=logging.INFO,
+            handlers=[
+                logging.FileHandler("finding_aid_lists.log"),
+                logging.StreamHandler(),
+            ],
+        )
 
-    def run(self):
-        repositories = {3: "nnc-a", 4: "nnc-ea", 5: "nnc-ut"}
-        for repo_id, repo_code in repositories.items():
-            resource_links = {}
-            for resource in self.as_client.published_resources(repo_id):
+    def create_all_lists(self):
+        """Creates html snippets of finding aid lists for all CUL repositories."""
+        logging.info("Starting process...")
+        try:
+            repositories = {3: "nnc-a", 4: "nnc-ea", 5: "nnc-ut"}
+            for repo_id, repo_code in repositories.items():
+                resource_links = {}
+                for resource in self.as_client.published_resources(repo_id):
+                    title = self.construct_title(resource)
+                    resource_link = f'<li><a href="/ead/{repo_code}/ldpd_{resource.id_0}">{title}</a></li>'
+                    resource_links[title] = resource_link
+                self.create_html_snippet(resource_links, repo_code)
+            rbml_links = {}
+            ua_links = {}
+            oh_links = {}
+            for resource in self.as_client.published_resources(2):
                 title = self.construct_title(resource)
-                resource_link = f'<li><a href="/ead/{repo_code}/ldpd_{resource.id_0}">{title}</a></li>'
-                resource_links[title] = resource_link
-            self.create_html_snippet(resource_links, repo_code)
-        rbml_links = {}
-        ua_links = {}
-        oh_links = {}
-        for resource in self.as_client.published_resources(2):
-            title = self.construct_title(resource)
-            rbml_code = "nnc-rb"
-            ua_code = "nnc-ua"
-            oh_code = "nnc-ccoh"
-            call_number = resource.json().get("user_defined", {}).get("string_1", "")
-            if call_number.startswith("UA"):
-                resource_link = f'<li><a href="/ead/{ua_code}/ldpd_{resource.id_0}">{title}</a></li>'
-                ua_links[title] = resource_link
-            elif call_number.startswith("OH"):
+                rbml_code = "nnc-rb"
+                ua_code = "nnc-ua"
+                oh_code = "nnc-ccoh"
+                call_number = (
+                    resource.json().get("user_defined", {}).get("string_1", "")
+                )
+                if call_number.startswith("UA"):
+                    resource_link = f'<li><a href="/ead/{ua_code}/ldpd_{resource.id_0}">{title}</a></li>'
+                    ua_links[title] = resource_link
+                elif call_number.startswith("OH"):
+                    resource_link = f'<li><a href="/ead/{oh_code}/ldpd_{resource.id_0}">{title}</a></li>'
+                    oh_links[title] = resource_link
+                else:
+                    resource_link = f'<li><a href="/ead/{rbml_code}/ldpd_{resource.id_0}">{title}</a></li>'
+                    rbml_links[title] = resource_link
+            self.create_html_snippet(rbml_links, rbml_code)
+            self.create_html_snippet(ua_links, ua_code)
+            for resource in self.as_client.published_resources(7):
+                title = self.construct_title(resource)
                 resource_link = f'<li><a href="/ead/{oh_code}/ldpd_{resource.id_0}">{title}</a></li>'
                 oh_links[title] = resource_link
-            else:
-                resource_link = f'<li><a href="/ead/{rbml_code}/ldpd_{resource.id_0}">{title}</a></li>'
-                rbml_links[title] = resource_link
-        self.create_html_snippet(rbml_links, rbml_code)
-        self.create_html_snippet(ua_links, ua_code)
-        for resource in self.as_client.published_resources(7):
-            title = self.construct_title(resource)
-            resource_link = (
-                f'<li><a href="/ead/{oh_code}/ldpd_{resource.id_0}">{title}</a></li>'
-            )
-            oh_links[title] = resource_link
-        self.create_html_snippet(oh_links, oh_code)
+            self.create_html_snippet(oh_links, oh_code)
+        except Exception as e:
+            logging.error(e)
 
     def create_html_snippet(self, links_dict, repo_code):
+        """Writes an HTML unordered list to a file.
+
+        links_dict (dict): finding aid titles (keys) and HTML link elements (values)
+        repo_code (str): CUL repository code (e.g., nnc-rb)
+        """
         links_dict = dict(sorted(links_dict.items()))
         with open(f"{self.base_path}/{repo_code}_fa_list.html", "w") as f:
             f.write("<ul>\n")
@@ -63,6 +83,10 @@ class FindingAidLists(object):
             f.write("</ul>")
 
     def construct_title(self, resource):
+        """Creates a finding aid title, including a formatted date.
+
+        resource (obj): ArchivesSnake resource object
+        """
         title = resource.title if resource.title.endswith(",") else f"{resource.title},"
         bulk_dates = []
         if resource.dates:
@@ -81,6 +105,11 @@ class FindingAidLists(object):
             return resource.title
 
     def format_date(self, date_json):
+        """Formats an ArchivesSpace data if date does not have a date expression.
+
+        Args:
+            date_json (dict): ArchivesSpace date
+        """
         if date_json.get("expression"):
             date_string = date_json["expression"]
         else:
