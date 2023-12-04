d997d87b1c4cd3bf4ffed4ded5e93a27491a03c8
Bonnie Gordon <blg2144@columbia.edu>
2023-12-04 10:29:16 -0500

    correct filenames

--- acfa_export_ead.py
+++ acfa_export_ead.py
@@ -15,7 +15,10 @@ def main():
     data_exporter = DataExporter()
     bibids = []
     for bibid, ead in data_exporter.export_resources(serialization="ead"):
-        ead_filepath = Path(ead_cache, f"as_ead_{bibid}.xml")
+        if bibid.isnumeric():
+            ead_filepath = Path(ead_cache, f"as_ead_ldpd_{bibid}.xml")
+        else:
+            ead_filepath = Path(ead_cache, f"as_ead_{bibid}.xml")
         with open(ead_filepath, "w") as ead_file:
             ead_file.write(ead)
         for matching_file in html_cache.glob(f"*{bibid}*"):
