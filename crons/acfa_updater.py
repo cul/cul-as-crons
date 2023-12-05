c252b64f62b3064fb698fd53cb78d1cacfbd55aa
Bonnie Gordon <blg2144@columbia.edu>
2023-12-05 17:32:48 -0500

    refactor acfa update

---
+++ crons/acfa_updater.py
@@ -0,0 +1,94 @@
+import logging
+from configparser import ConfigParser
+from pathlib import Path
+
+import requests
+
+from .aspace_client import ArchivesSpaceClient
+from .helpers import yesterday_utc
+
+
+class UpdateAllInstances(object):
+    def __init__(self, parent_cache):
+        """If no date is provided, default to 24 hours ago.
+
+        Expects a config file with (only) an unlimited number of repositories in the following format:
+
+        [Instance_Name]
+        baseurl: https://sandbox.archivesspace.org/api/
+        username: admin
+        password: admin
+        """
+        current_path = Path(__file__).parents[1].resolve()
+        config_file = Path(current_path, "as_export.cfg")
+        self.config = ConfigParser()
+        self.config.read(config_file)
+        self.parent_cache = parent_cache
+
+    def all_repos(self):
+        """Iterates through each repository in each ASpace instance."""
+        for instance_name in self.config.sections():
+            as_client = ArchivesSpaceClient(
+                self.config[instance_name]["baseurl"],
+                self.config[instance_name]["username"],
+                self.config[instance_name]["password"],
+            )
+            for repo in as_client.aspace.repositories:
+                UpdateRepository(as_client, repo).daily_update()
+
+
+class UpdateRepository(object):
+    def __init__(self, as_client, repo, parent_cache):
+        self.export_params = {
+            "include_unpublished": False,
+            "include_daos": True,
+        }
+        self.acfa_base_url = "https://findingaids.library.columbia.edu/ead/"
+        self.as_client = as_client
+        self.repo = repo
+        self.ead_cache = Path(parent_cache, "ead_cache")
+        self.html_cache = Path(parent_cache, "html_cache")
+
+    def daily_update(self, timestamp=None):
+        """Updates EAD and HTML caches, updates index."""
+        timestamp = yesterday_utc() if timestamp is None else timestamp
+        for resource in self.updated_resources(timestamp):
+            ead = self.as_client.aspace.client.get(
+                f"/repositories/{self.repo.id}/resource_descriptions/{resource.id}.xml",
+                params=self.export_params,
+            ).content.decode("utf-8")
+            bibid = f"{resource.id_0}{getattr(resource, 'id_1', '')}"
+            if bibid.isnumeric():
+                ead_filepath = Path(self.ead_cache, f"as_ead_ldpd_{bibid}.xml")
+            else:
+                ead_filepath = Path(self.ead_cache, f"as_ead_{bibid}.xml")
+            with open(ead_filepath, "w") as ead_file:
+                ead_file.write(ead)
+            for matching_file in self.html_cache.glob(f"*{bibid}*"):
+                if matching_file.suffix == ".html":
+                    matching_file.unlink()
+            self.crawl_finding_aid(resource, self.repo.org_code.lower())
+            # TODO: trigger reindex
+
+    def updated_resources(self, timestamp):
+        resource_ids = self.as_client.aspace.client.get(
+            f"/repositories/{self.repo.id}/resources",
+            params={"all_ids": True, "modified_since": timestamp},
+        ).json()
+        for resource_id in resource_ids:
+            resource = self.repo.resources(resource_id)
+            if resource.publish and not resource.suppressed:
+                yield resource
+
+    def crawl_finding_aid(self, resource, repo_code):
+        fa_url = f"{self.acfa_base_url}{repo_code}/ldpd_{resource.id_0}"
+        requests.get(fa_url)
+        requests.get(f"{fa_url}/dsc")
+        series_num = 1
+        children = self.as_client.aspace.client.get(
+            f"{resource.uri}/tree/waypoint?offset=0"
+        ).json()
+        for x in range(len(children)):
+            requests.get(f"{fa_url}/dsc/{series_num}")
+            series_num += 1
+        logging.info(f"Requested {resource.title} ({resource.id_0})")
