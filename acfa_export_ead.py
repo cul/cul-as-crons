4d764fb8c5fb320b371426b645434763bcda84f3
Bonnie Gordon <blg2144@columbia.edu>
2024-02-15 16:23:49 -0500

    add api key to arguments

--- acfa_export_ead.py
+++ acfa_export_ead.py
@@ -7,9 +7,10 @@ def main():
     parser = argparse.ArgumentParser(
         description="Exports EAD files from all ASpace instances in config file to a directory and deletes related HTML files"
     )
+    parser.add_argument("api_key", help="API key for finding aids API")
     parser.add_argument("parent_cache", help="Parent directory of EAD and HTML caches")
     args = parser.parse_args()
-    UpdateAllInstances(args.parent_cache).all_repos()
+    UpdateAllInstances(args.parent_cache).all_repos(args.api_key)
 
 
 if __name__ == "__main__":
