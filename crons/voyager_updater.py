aed6437b0c0d09a5f5f8d16ad4f745394e997e17
Bonnie Gordon <blg2144@columbia.edu>
2024-08-21 17:23:27 -0400

    mostly working code for CUL collections

--- crons/voyager_updater.py
+++ crons/voyager_updater.py
@@ -38,6 +38,12 @@ class UpdateAllInstances(object):
 
 class UpdateRepository(object):
     def __init__(self, as_client, repo):
+        """Initializes an UpdateRepository instance.
+
+        Args:
+            as_client (ArchivesSpaceClient): ArchivesSpace client instance.
+            repo (ArchivesSpace.Repository): ArchivesSpace repository object.
+        """
         self.export_params = {
             "include_unpublished": False,
         }
@@ -45,7 +51,14 @@ class UpdateRepository(object):
         self.repo = repo
 
     def updated_marc(self, timestamp=None):
-        """Gets MARCXML for recently updated records."""
+        """Gets MARCXML for recently updated records.
+
+        Args:
+            timestamp (str, optional): The timestamp to filter resources by modification date. Defaults to yesterday_utc().
+
+        Yields:
+            Generator: MARCXML content.
+        """
         timestamp = yesterday_utc() if timestamp is None else timestamp
         for resource in self.updated_resources(timestamp):
             bibid = self.get_bibid(resource)
@@ -68,6 +81,14 @@ class UpdateRepository(object):
                     print(bibid, e)
 
     def updated_resources(self, timestamp):
+        """Retrieves recently updated resources from the repository.
+
+        Args:
+            timestamp (str): The timestamp to filter resources by modification date.
+
+        Yields:
+            Generator: ArchivesSpace resource objects.
+        """
         resource_ids = self.as_client.aspace.client.get(
             f"/repositories/{self.repo.id}/resources",
             params={"all_ids": True, "modified_since": timestamp},
@@ -78,6 +99,14 @@ class UpdateRepository(object):
                 yield resource
 
     def get_bibid(self, resource):
+        """Retrieves the bibid from a resource.
+
+        Args:
+            resource (ArchivesSpace.Resource): ArchivesSpace resource object.
+
+        Returns:
+            str: The bibid if found, otherwise False.
+        """
         if resource.id_0.isnumeric():
             return resource.id_0
         elif getattr(resource, "user_defined"):
@@ -96,13 +125,16 @@ class MarcRecord(object):
         """Set up MARC record.
 
         Args:
+            bibid (str): bibid of resource
             marc_response (obj): response from ASpace API
         """
-        self.bibid = bibid
+        self.bibid = str(bibid)
+        self.marc_response = marc_response
         self.marc_record = etree.fromstring(marc_response.content)
         self.namespaces = {None: "http://www.loc.gov/MARC21/slim"}
 
     def validate_marc(self):
+        """Validates a MARC record against the MARC21slim schema."""
         validate_against_schema(self.marc_response.content, "MARC21slim")
 
     def all_records(self):
@@ -111,7 +143,18 @@ class MarcRecord(object):
         # QUESTION: do we need to change namespace?
         pass
 
+    def process_cul_record(self):
+        print("Procesing...")
+        self.add_controlfield_001()
+        self.add_controlfield_003()
+        self.update_datafield_035_culaspc()
+        self.update_datafield_100()
+        self.update_datafield_856()
+        self.add_965noexportAUTH()
+        self.corpname_punctuation()
+
     def add_controlfield_001(self):
+        """Adds bibid to controlfield 001."""
         if not self.marc_record.find(
             ".//controlfield[@tag='001']", namespaces=self.namespaces
         ):
@@ -119,17 +162,22 @@ class MarcRecord(object):
                 ".//leader", namespaces=self.namespaces
             )
             leader_element_index = leader_element.getparent().index(leader_element)
-            controlfield_001 = etree.Element("controlfield", tag="001")
+            controlfield_001 = etree.Element(
+                "{http://www.loc.gov/MARC21/slim}controlfield", tag="001"
+            )
             controlfield_001.text = self.bibid
             leader_element.getparent().insert(
                 leader_element_index + 1, controlfield_001
             )
 
     def add_controlfield_003(self):
+        """Adds NNC to controlfield 003 and inserts after controlfield 001."""
         if not self.marc_record.find(
             ".//controlfield[@tag='003']", namespaces=self.namespaces
         ):
-            controlfield_003 = etree.Element("controlfield", tag="003")
+            controlfield_003 = etree.Element(
+                "{http://www.loc.gov/MARC21/slim}controlfield", tag="003"
+            )
             controlfield_003.text = "NNC"
             controlfield_001 = self.marc_record.find(
                 ".//controlfield[@tag='001']", namespaces=self.namespaces
@@ -155,7 +203,7 @@ class MarcRecord(object):
         subfield_d = datafield_100.find(
             './subfield[@code="d"]', namespaces=self.namespaces
         )
-        if subfield_d:
+        if subfield_d is not None:
             subfield_d.text = subfield_d.text.rstrip(
                 ",."
             )  # Remove trailing comma or period
@@ -170,29 +218,63 @@ class MarcRecord(object):
         datafield_856 = self.marc_record.find(
             './/datafield[@tag="856"]', namespaces=self.namespaces
         )
-        subfield_z = datafield_856.find(
-            './subfield[@code="z"]', namespaces=self.namespaces
-        )
-        if subfield_z:
-            datafield_856.remove(subfield_z)
-        subfield_3 = etree.Element("subfield", code="3")
-        subfield_3.text = "Finding aid"
-        datafield_856.append(subfield_3)
+        if datafield_856 is not None:
+            subfield_z = datafield_856.find(
+                './subfield[@code="z"]', namespaces=self.namespaces
+            )
+            if subfield_z:
+                datafield_856.remove(subfield_z)
+            subfield_3 = etree.Element(
+                "{http://www.loc.gov/MARC21/slim}subfield", code="3"
+            )
+            subfield_3.text = "Finding aid"
+            datafield_856.append(subfield_3)
 
     def add_965noexportAUTH(self):
-        datafield_965 = etree.Element("datafield", tag="965")
-        subfield_a = etree.Element("subfield", code="a")
+        datafield_965 = etree.Element(
+            "{http://www.loc.gov/MARC21/slim}datafield", tag="965"
+        )
+        subfield_a = etree.Element("{http://www.loc.gov/MARC21/slim}subfield", code="a")
         subfield_a.text = "965noexportAUTH"
         datafield_965.append(subfield_a)
         self.marc_record[0].append(datafield_965)
 
-    def cul(self):
-        # add 965noexportAUTH to 965
-        # update 856
-        # make sure bibid is 001 tag, not 099 tag
-        # <controlfield tag="003">NNC</controlfield>
-        # reformat 035 CULASPC to local practice
-        pass
+    def corpname_punctuation(self):
+        """Processes corpname punctuation in 110 and 610 datafields."""
+        datafield_110 = self.marc_record.find(
+            './/datafield[@tag="110"]', namespaces=self.namespaces
+        )
+        if datafield_110:
+            self.process_corpname_datafield(datafield_110)
+        datafield_610_all = self.marc_record.findall(
+            './/datafield[@tag="610"]', namespaces=self.namespaces
+        )
+        for datafield_610 in datafield_610_all:
+            self.process_corpname_datafield(datafield_610)
+
+    def process_corpname_datafield(self, datafield):
+        """Processes a corpname datafield (110 or 610) to remove trailing punctuation from subfields a and b.
+
+        Args:
+            datafield (etree.Element): The corpname datafield element.
+        """
+        subfields_a = datafield.findall(
+            './/subfield[@code="a"]', namespaces=self.namespaces
+        )
+        if subfields_a:
+            if not datafield.findall(
+                './/subfield[@code="b"]', namespaces=self.namespaces
+            ):
+                subfield_a = subfields_a[0]
+                if subfield_a.text.endswith((".", ",")):
+                    subfield_a.text = subfield_a.text[:-1]
+            else:
+                subfields_b = datafield.findall(
+                    './/subfield[@code="b"]', namespaces=self.namespaces
+                )
+                subfield_b = subfields_b[0]
+                if subfield_b.text.endswith(","):
+                    subfield_b.text = subfield_b.text[:-1]
 
     def barnard(self):
         # TODO: ask LIT - are we adding 003 for barnard?
